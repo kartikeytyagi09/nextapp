4999790272e8048ef4fc29b4c88f2a84bdda6213
kartikey tyagi <tkartikey92@gmail.com>
2025-11-08 00:38:18 +0530

    many new things idk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Toaster } from 'sonner';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import AuthProvider from './context/AuthProvider';
+import AuthProvider from '../context/AuthProvider';
 
 
 const inter = Inter({ subsets: ['latin'] });
